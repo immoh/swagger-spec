b97d25cd55d479be151ae130cb7a807c041735d2
Immo Heikkinen <immo.heikkinen@gmail.com>
2017-05-27 08:53:39 +0300

    Bump cheshire version

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha17" :scope "provided"]
                  [org.clojure/spec.alpha "0.1.123"]
                  [circleci/clj-yaml "0.5.5"]
-                 [cheshire "5.7.0"]]
+                 [cheshire "5.7.1"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
